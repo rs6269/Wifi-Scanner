6e5cc93c60ae256405ef2d4780d6f4638df7fdf7
rs6269 <rs6269@g.rit.edu>
2026-05-14 12:28:41 +0400

    Delete wifi-scanner-release directory

--- wifi-scanner-release/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Building WiFi Security Scanner...
-py -m PyInstaller --noconfirm --onefile --windowed --name WiFiScanner app.py
-echo Build complete! Executable is in the dist folder.
-pause
